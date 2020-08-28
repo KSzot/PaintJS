3311a8f93a5bd57fe82b9cd412a2cf839355f8f1
Krystian <k.szot@nextrope.com>
2020-08-28 19:58:32 +0200

    Appearance button and field canvas done

--- src/js/Modal/storage.js
+++
@@ -1,6 +0,0 @@
-export function getLocation() {
-  return localStorage.getItem('nameCity');
-}
-export function setLocation(name) {
-  localStorage.setItem('nameCity', name);
-}
--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,5 @@
-import '../css/main.scss';
 import 'bootstrap';
-import View from './Views/View';
-import { getWeather } from './Modal/apiCall';
-import { getLocation, setLocation } from './Modal/storage';
-
-class Controller {
-  constructor(View) {
-    this.view = View;
-    this.initWeather();
-    this.view.bindChangeCity(this.onHandleChangeCity);
-  }
-  initWeather() {
-    if (getLocation() === null) setLocation('London');
-    getWeather(getLocation())
-      .then((res) => this.view.displayWeather(res))
-      .catch((err) => console.log(err));
-  }
-  onHandleChangeCity(obj, ondisplayWeather, onTooltip, onCloseModal) {
-    getWeather(obj)
-      .then((res) => {
-        ondisplayWeather(res);
-        onCloseModal();
-      })
-      .catch((err) => onTooltip(err));
-  }
-}
-
-const app = new Controller(new View());
-document.addEventListener('DOMContentLoaded', app);
+import '../css/main.scss';
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Hello World');
+});
